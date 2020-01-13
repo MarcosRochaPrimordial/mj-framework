b5b713f11de01685110b93ef5d059cd95d1ec488
MarcosRochaPrimordial <markossdeath@gmail.com>
2020-01-13 13:30:44 -0300

    Adding version flag

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import arg from 'arg';
 import inquirer from 'inquirer';
 import { createProject, addController } from './main';
 import packageJsonInfo from './../package.json';
+import chalk from 'chalk';
 
 function parseArgumentsIntoOptions(rawArgs) {
     const args = arg(
@@ -23,23 +24,28 @@ function parseArgumentsIntoOptions(rawArgs) {
 async function promptForMissingOptions(options) {
     const questions = [];
     options.actions = options.actions === 'new' || options.actions === 'create' ? options.actions : undefined;
-    if (!options.actions && !options.version) {
-        questions.push({
-            type: 'list',
-            name: 'actions',
-            message: 'Choose what you want to do',
-            choices: ['new', 'create'],
-            default: 'new'
-        })
-    }
+    if (!options.actions) {
+        console.log(
+`
+Usage: mj <command>
 
-    if (!options.nameAction) {
-        questions.push({
-            type: 'input',
-            name: 'nameAction',
-            message: 'name your action',
-            default: 'Name'
-        });
+commands:
+    --version: console the version
+    -v: alias --version
+    new <project-name>: create a new project
+    create <controller-name>: create a new controller and service
+
+MJ-Framework@${packageJsonInfo.version}
+`);
+    } else {
+        if (!options.nameAction) {
+            questions.push({
+                type: 'input',
+                name: 'nameAction',
+                message: 'name your action',
+                default: 'Name'
+            });
+        }
     }
 
     const answers = await inquirer.prompt(questions);
@@ -53,12 +59,39 @@ async function promptForMissingOptions(options) {
 export async function cli(args) {
     let options = parseArgumentsIntoOptions(args);
     if (options.version) {
-        console.log(
-`MJ-Framework is in the version: ${packageJsonInfo.version}
+//         console.log(
+// `MJ-Framework is in the version: ${packageJsonInfo.version}
 
-github: https://github.com/MarcosRochaPrimordial/mj-framework
+// github: https://github.com/MarcosRochaPrimordial/mj-framework
+// `);
+
+console.log('%s %s',
+chalk.yellow.bold(
 `
-        );
+                      MJ-Framework is in the version: ${packageJsonInfo.version}`),
+chalk.magenta.bold(
+`
+                                 _______ _________
+                                (       )\\__    _/
+                                | () () |   )  (  
+                                | || || |   |  |  
+                                | |(_)| |   |  |  
+                                | |   | |   |  |  
+                                | )   ( ||\\_)  )  
+                                |/     \\|(____/
+ _______  _______  _______  _______  _______           _______  _______  _       
+(  ____ \\(  ____ )(  ___  )(       )(  ____ \\|\\     /|(  ___  )(  ____ )| \\    /\\
+| (    \\/| (    )|| (   ) || () () || (    \\/| )   ( || (   ) || (    )||  \\  / /
+| (__    | (____)|| (___) || || || || (__    | | _ | || |   | || (____)||  (_/ / 
+|  __)   |     __)|  ___  || |(_)| ||  __)   | |( )| || |   | ||     __)|   _ (  
+| (      | (\\ (   | (   ) || |   | || (      | || || || |   | || (\\ (   |  ( \\ \\ 
+| )      | ) \\ \\__| )   ( || )   ( || (____/\\| () () || (___) || ) \\ \\__|  /  \\ \\
+|/       |/   \\__/|/     \\||/     \\|(_______/(_______)(_______)|/   \\__/|_/    \\/
+`
+), `
+
+github: https://github.com/MarcosRochaPrimordial/mj-framework`);
+
     } else {
         options = await promptForMissingOptions(options);
         if (options.actions === 'new') {
